884f78610cededc8ad28fdbc57f0a0575c3c9ef4
Nokky07 <kieranmcm2007@gmail.com>
2024-01-17 20:09:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import random
+import menu
+import colours
+
+
+
+
+def main_menu():  # Menu Start
+   
+  
+  print(colours.bright_magenta + menu.header)
+
+
+
+main_menu()  # Call function
