f282f873c5b22ed8118065b72d3ca16921f2bc2d
Nokky07 <kieranmcm2007@gmail.com>
2024-01-17 20:27:21 +0000

    Basically setting up menu but while loop is usually being a pain. Fix it later for now.

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import random
 import menu
 import colours
 
-
-
-
 def main_menu():  # Menu Start
-   
-  
-  print(colours.bright_magenta + menu.header)
+    
+    print(colours.bright_magenta + menu.header)
+    print(colours.bright_cyan + menu.options)
 
+    choose = input("Choose an option: ") 
+    
+    while choose not in range(1, 10):
+        
+        print("That is not a valid option.")
+        
+        choose = input("Choose an option: ")  
+        
+        if int(choose) in range(1, 10):
+            break
 
-
-main_menu()  # Call function
+main_menu()  
--- menu.py
+++ menu.py
@@ -6,3 +6,35 @@ a88aaaa8P' dP    dP d8888P 88d888b. .d8888b. 88d888b.       88    dP    dP 88d88
  dP        `8888P88   dP   dP    dP `88888P' dP    dP       dP    `8888P88 88Y888P' `88888P' 
                 .88                                                    .88 88                
             d8888P                                                 d8888P  dP                 '''
+
+
+
+
+
+
+
+
+
+level_heading = '''    __                   __    
+   / /   ___ _   _____  / /____
+  / /   / _ \ | / / _ \/ / ___/
+ / /___/  __/ |/ /  __/ (__  ) 
+/_____/\___/|___/\___/_/____/'''
+
+
+
+
+
+
+options = '''
+
+    Beginner                                Intermediate
+
+    (1) - Hello World!                      (7) - For Loops
+    (2) - Addition/Subtraction
+    (3) - Variables
+    (4) - Lists
+    (5) - Tuples
+    (6) - Dictionarys
+    
+'''
