401c87255978a8acf841adc1b6567dbc89bde787
Nokky07 <kieranmcm2007@gmail.com>
2024-01-18 17:05:40 +0000

    .

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
 import time
 import random
 import art
-import colours
+from colours import *
 import sys
+import replit
+
 
 
 def hello_world():
-  print("chese")
+  input(f"\n{darken}Press {bright_blue}[{Blue}ENTER{bright_blue}]{reset} {darken}To Start{reset} ")
 
 
 def main_menu():  # Menu Start
-
-  print(colours.Green + "                        " +
+  
+  print(Green + "                        " +
         f"\n\n\n\n\n\n\n\n{art.level_heading}")
 
-  print(colours.bright_cyan + art.options)
+  print(bright_cyan + art.options)
 
   user = int(input("Choose an option: "))
 
   while user not in range(1, 10):
 
-    print(colours.Red + "\n\n      _-_-_-Invalid option-_-_-_      \n\n")
+    print(Red + "\n\n      _-_-_-Invalid option-_-_-_      \n\n")
 
-    user = int(input(colours.bright_cyan + "\n\nChoose an option: "))
+    user = int(input(bright_cyan + "\n\nChoose an option: "))
 
     if user in range(1, 10):
       break
-
+  
+  
+  print(Red + "\n\nWhile Loop has intially been broken\n\n"
+    )  # Developer purposes will be removed later on in implementation.
+  
+  
   if user == 1:
+    replit.clear()
     hello_world()
 
-  print(colours.Red + "\n\nWhile Loop has intially been broken\n\n"
-        )  # Developer purposes will be removed later on in implementation.
 
 
-print(colours.bright_magenta + art.header)
+
+print(bright_magenta + art.header)
 print(
-    f"\n{colours.darken}{colours.italic}Created by {colours.reset}{colours.Orange}Nokky07{colours.reset}{colours.darken}'s {colours.reset}{colours.bright_yellow}Standard Type{colours.reset}{colours.darken}...{colours.reset}"
+    f"\n{darken}{italic}Created by {reset}{Orange}Nokky07{reset}{darken}'s {reset}{bright_yellow}Standard Type{reset}{darken}...{reset}"
 )
 
 main_menu()
