15ed60b0b66a9aa6a05b8dcf2dd6118793dcf8c2
Nokky07 <kieranmcm2007@gmail.com>
2024-01-19 11:23:45 +0000

    blah blop

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import sys
 import replit
 
 
-
 def hello_world():
-  input(f"\n{darken}Press {bright_blue}[{Blue}ENTER{bright_blue}]{reset} {darken}To Start{reset} ")
+  input(
+      f"\n{darken}Press {bright_blue}[{Blue}ENTER{bright_blue}]{reset} {darken}To Start{reset} "
+  )
 
 
 def main_menu():  # Menu Start
-  
+
   print(Green + "                        " +
         f"\n\n\n\n\n\n\n\n{art.level_heading}")
 
@@ -28,17 +29,13 @@ def main_menu():  # Menu Start
 
     if user in range(1, 10):
       break
-  
-  
+
   print(Red + "\n\nWhile Loop has intially been broken\n\n"
-    )  # Developer purposes will be removed later on in implementation.
-  
-  
+        )  # Developer purposes will be removed later on in implementation.
+
   if user == 1:
     replit.clear()
     hello_world()
-
-
 
 
 print(bright_magenta + art.header)
@@ -47,4 +44,3 @@ print(
 )
 
 main_menu()
-hello_world()
